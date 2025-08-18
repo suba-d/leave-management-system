37001e39a975fde7c514965703795366425ff7f9
suba <david@appssp.com>
2025-08-18 15:51:52 +0800

    第二次部署

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+if [ -z "$1"]; then
+    echo "錯誤：需要提供commit message"
+    echo "用法：./depoly.sh\"你的更新說名\""
+    exit 1
+fi
+
+
+
 git add .
 git commit -m "$1"
 git push origin main
