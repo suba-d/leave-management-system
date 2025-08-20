f0d94b7db197ff024efd8ab9553217c497478215
suba <david@appssp.com>
2025-08-20 15:30:13 +0800

    美化ui介面

---
+++ migrations/versions/af3154f625c0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: af3154f625c0
+Revises: 9d0e455da701, c87e82b48819
+Create Date: 2025-08-20 06:51:05.046284
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'af3154f625c0'
+down_revision = ('9d0e455da701', 'c87e82b48819')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
